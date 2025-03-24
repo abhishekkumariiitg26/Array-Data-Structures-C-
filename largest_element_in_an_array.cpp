7eacf112aeb1e0f3cfd653aed32fdbd88b4f58ee
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-24 09:46:05 +0530

    inital commit

--- largest_element_in_an_array.cpp
+++ largest_element_in_an_array.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 using namespace std;
 
 vector<int> build_array(vector<int> &array){
+  
     cout<<"Enter the elements of the array: ";
     for (int i = 0; i < array.size(); i++)
     {
@@ -12,17 +14,42 @@ vector<int> build_array(vector<int> &array){
     
 }
 void print_array(vector<int> &array){
+   
     cout<<"The elements of the array are: ";
     for(int item:array){
         cout<<item<<" ";
     }
 }
+
+int find_largest_element(vector<int> &array){
+    
+    
+    int ritik=INT_MIN;
+    for(int item:array){
+        if (ritik<item)
+        {
+            ritik=item;
+        }
+        
+    }
+    return ritik;
+}
+
 int main(){
     int size;
     cout<<"Enter the size of the array: ";
     cin>>size;
     vector<int> array(size);
-    build_array(array);
-    print_array(array);
+    if (array.size()==0)
+    {
+        cout<<"The array is empty."<<endl;
+    }
+    else{
+
+        build_array(array);
+        print_array(array);
+        int largest=find_largest_element(array);
+        cout<<endl<<"The largest value in the array is: "<<largest;
+    }
     return 0;
 }
