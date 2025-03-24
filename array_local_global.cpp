868bae3816668e74d8e04e10091fe6e37e1d691e
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-24 09:09:12 +0530

    comment added

--- array_local_global.cpp
+++ array_local_global.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
-int array_global[10]; // initialised with 0
+int array_global[10]; // initialised with 0 and max size is 10^7
 int main(){
-    int array_local[10]; // initialised with garbage value
+    int array_local[10]; // initialised with garbage value and max size can be 10^6
     for (int i = 0; i < 10; i++)
     {
         cout<<array_global[i]<<" ";
